fa7c3a350d374fadbabed4e9692d15342f588bff
dzianis <dzianis.makeichyk@gmail.com>
2017-11-23 10:05:56 +0100

    add scss

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,24 @@
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+    methods: {
+      loaders: [
+        {
+          test: /\.vue$/,
+          loader: 'vue'
+        },
+        {
+          test: /\.s[a|c]ss$/,
+          loader: 'style!css!sass'
+        },
+      ]
+    },
+    vue: {
+      loaders: {
+      scss: 'style!css!sass'
+    }
+  }
 }
 </script>
 
